c8abc8354491cf8ab061b0679308491a3a3c4343
David Jiménez <thebanusco10@gmail.com>
2025-06-07 18:21:43 +0200

    Base app

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/icon'],
+  css: ["~/assets/app.css"],
+})
---
+++ pages/token/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { code } = useRoute().query;
+
+if (code) {
+    const { data, error } = await useFetch("/api/auth", {
+        method: "POST",
+        query: {
+            code
+        },
+    });
+
+    console.log(data.value);
+    console.log(error.value?.message);
+}
+
+</script>
+
+<template>
+    <main>
+        <p>Será redirigido en unos momentos...</p>
+    </main>
+</template>
